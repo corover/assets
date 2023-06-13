550c17c9a4d0b9ca922a322f54d59de6124661c5
sudhanshu956 <57168249+sudhanshu956@users.noreply.github.com>
2023-06-13 13:55:57 +0530

    adding all context-bot

---
+++ assets_corover/data/searchBoxComponentData.ts
@@ -0,0 +1,10 @@
+export const whatsappData = {
+  en: "Get Connected On WhatsApp",
+  hi: "व्हाट्सएप पर जुड़ें",
+  ta: "வாட்ஸ்அப்பில் இணைக்கவும்",
+  te: "WhatsAppలో కనెక్ట్ అవ్వండి",
+  ar: "تواصل مع WhatsApp",
+  ja: "WhatsAppでつながる",
+  fr: "Connectez-vous sur WhatsApp",
+  in: "Terhubung Di WhatsApp",
+};
---
+++ digisaathi_assets/languageDatabase.ts
@@ -0,0 +1,26 @@
+export const language = {
+  en: {
+    header: {},
+    home: {},
+    searchBar: {},
+    footer: {},
+  },
+  hi: {
+    header: {},
+    home: {},
+    searchBar: {},
+    footer: {},
+  },
+  mr: {
+    header: {},
+    home: {},
+    searchBar: {},
+    footer: {},
+  },
+  kn: {
+    header: {},
+    home: {},
+    searchBar: {},
+    footer: {},
+  },
+};
