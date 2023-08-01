05babfeb69702ddbbe8b6abb154364fb0249902d
sudhanshu956 <57168249+sudhanshu956@users.noreply.github.com>
2023-08-01 13:04:48 +0530

    +skill india assets

---
+++ context-video-assets/assets/fallback.ts
@@ -0,0 +1,225 @@
+export const fallback = {
+   en: [
+      {
+         id: "1",
+         status: 404,
+         answer: "Good question I will get back to you",
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/8a9c6c24-5f05-4d46-885c-e1a5788817e1_en.mp3",
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/En-FallBack-GoodQuestion-Getbacktoyou.m4v",
+         video: true,
+         answerId: " ",
+      },
+      {
+         id: "1",
+         status: 404,
+         answer: "Can you please ask me any other question",
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/fed2a008-d5b0-4078-9790-5e054202c76f_en.mp3",
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/En-FallBack-AskOtherQuestion.m4v",
+         video: true,
+         answerId: " ",
+      },
+      {
+         id: "1",
+         status: 404,
+         answer: "Please Ask me any relevent question",
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/cca6cc5f-0151-4407-ba72-cf61490a340b_en.mp3",
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/En-FallBack-AskAnyrelevent-Question.m4v",
+         video: true,
+         answerId: " ",
+      },
+      {
+         id: "1",
+         status: 404,
+         answer:
+            "Sorry I Don't have the answer currently you can please share your contact details we will get back to you",
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/226a7468-8e3e-4aa8-bd04-ab610fbd9ffb_en.mp3",
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/En-FallBack-Sorry-DontHaveAnswer-GetBackToYou.m4v",
+         video: true,
+         answerId: " ",
+      },
+      {
+         id: "1",
+         status: 404,
+         answer: "Please ask me Any other question",
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/44e989a1-10a8-4169-8163-2b10d24f2520_en.mp3",
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/En-FallBack-Any-other-question-please.m4v",
+         video: true,
+         answerId: " ",
+      },
+      {
+         id: "1",
+         status: 404,
+         answer: "I will get back to you",
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/bb66b72e-fe12-439e-b33d-633b7d45e645_en.mp3",
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/En-FallBack-I-Will-GetBack-ToYou.m4v",
+         video: true,
+         answerId: " ",
+      },
+      {
+         id: "1",
+         status: 404,
+         answer: "I am facing connectivity issue please try after some time",
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/8d7dbdc1-375a-4ebd-bb87-1a9b5f609907_en.mp3",
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/En-FallBack-IamFacing-ConnectivityIssue.m4v",
+         video: true,
+         answerId: " ",
+      },
+      {
+         id: "1",
+         status: 404,
+         answer: "If you have more question visit our website",
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/42bb3b53-c25c-4049-ad58-b030e85f6cd8_en.mp3",
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/En-FallBack-MoreQuestion-Visitwebsite.m4v",
+         video: true,
+         answerId: " ",
+      },
+      {
+         id: "1",
+         status: 404,
+         answer: "Sorry",
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/3ef1c4dd-c4c2-4f65-bdcb-bdf69871a539_en.mp3",
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/En-FallBack-Sorry.m4v",
+         video: true,
+         answerId: " ",
+      },
+      {
+         id: "1",
+         status: 404,
+         answer: "You can please write to us by a Email we will get back to you",
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/6de04254-986f-4f4d-906e-d1fb05807b06_en.mp3",
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/En-FallBack-Writeus-Email.m4v",
+         video: true,
+         answerId: " ",
+      },
+   ],
+   hi: [
+      {
+         id: "1",
+
+         status: 404,
+
+         answer: "अच्छा सवाल है हम आप से शीघ्र मिलेंगे",
+
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/8d2697e9-53c8-4d22-84b0-d657063c89b3_hi.mp3",
+
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/Hi-Fallback-%E0%A4%85%E0%A4%9A%E0%A5%8D%E0%A4%9B%E0%A4%BE-%E0%A4%B8%E0%A4%B5%E0%A4%BE%E0%A4%B2-%E0%A4%86%E0%A4%AA%E0%A4%B8%E0%A5%87-%E0%A4%AE%E0%A4%BF%E0%A4%B2%E0%A4%A4%E0%A4%BE-%E0%A4%B9%E0%A5%88.mp4",
+
+         video: true,
+
+         answerId: " ",
+      },
+
+      {
+         id: "1",
+
+         status: 404,
+
+         answer: "कृपया कोई अन्य प्रश्न पूछें",
+
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/362aa81e-f704-4904-b954-97b453e91f23_hi.mp3",
+
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/Hi-FallBack-कृपया-कोई-अन्य-प्रश्न-पूछें.m4v",
+
+         video: true,
+
+         answerId: " ",
+      },
+
+      {
+         id: "1",
+
+         status: 404,
+
+         answer: "हम आपसे शीघ्र ही संपर्क करेंगे",
+
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/11777e38-2553-42a8-b6dd-657f522d4e00_hi.mp3",
+
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/Hi-Fallbck-%E0%A4%B9%E0%A4%AE-%E0%A4%86%E0%A4%AA%E0%A4%B8%E0%A5%87-%E0%A4%B6%E0%A5%80%E0%A4%98%E0%A5%8D%E0%A4%B0-%E0%A4%B9%E0%A5%80-%E0%A4%B8%E0%A4%82%E0%A4%AA%E0%A4%B0%E0%A5%8D%E0%A4%95-%E0%A4%95%E0%A4%B0%E0%A5%87%E0%A4%82%E0%A4%97%E0%A5%87.mp4",
+
+         video: true,
+
+         answerId: " ",
+      },
+
+      {
+         id: "1",
+
+         status: 404,
+
+         answer: "मैं अभी इस प्रश्न का उत्तर देने में सक्षम नहीं हूं",
+
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/02f8df53-d029-4eb4-bf3d-b96f825af4d4_hi.mp3",
+
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/Hi-Fallback-%E0%A4%AE%E0%A5%88%E0%A4%82-%E0%A4%85%E0%A4%AD%E0%A5%80-%E0%A4%87%E0%A4%B8-%E0%A4%AA%E0%A5%8D%E0%A4%B0%E0%A4%B6%E0%A5%8D%E0%A4%A8-%E0%A4%95%E0%A4%BE-%E0%A4%89%E0%A4%A4%E0%A5%8D%E0%A4%A4%E0%A4%B0-%E0%A4%A6%E0%A5%87%E0%A4%A8%E0%A5%87-%E0%A4%AE%E0%A5%87%E0%A4%82-%E0%A4%B8%E0%A4%95%E0%A5%8D%E0%A4%B7%E0%A4%AE-%E0%A4%A8%E0%A4%B9%E0%A5%80%E0%A4%82-%E0%A4%B9%E0%A5%82%E0%A4%82.mp4",
+
+         video: true,
+
+         answerId: " ",
+      },
+
+      {
+         id: "1",
+
+         status: 404,
+
+         answer: "आप इस प्रश्न को ईमेल आईडी पर भी भेज सकते हैं हम आप से शीघ्र ही संपर्क करेंगे",
+
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/41ae7abd-e0e7-4852-9abb-976a4429e55f_hi.mp3",
+
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/Hi-Fallback-आप-इस-प्रश्न-को-ईमेल-आईडी-पर-भी-भेज-सकते-हैं-हम-आप-से-शीघ्र-ही-संपर्क-करेंगे.mp4",
+
+         video: true,
+
+         answerId: " ",
+      },
+
+      {
+         id: "1",
+
+         status: 404,
+
+         answer: "यदि आपके पास अधिक प्रश्न है तो आप हमारे वेबसाइट पर जाएं",
+
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/15b0c5aa-3f3b-40e2-9681-0603154233d2_hi.mp3",
+
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/Hi-Fallbck-यदि-आपके-पास-अधिक-प्रश्न-है-तो-आप-हमारे-वेबसाइट-पर-जाएं.mp4",
+
+         video: true,
+
+         answerId: " ",
+      },
+
+      {
+         id: "1",
+
+         status: 404,
+
+         answer: "हम आपसे शीघ्र मिलेंगे",
+
+         audio: "https://coroverbackendstorage.blob.core.windows.net/chatbot-audio-bucket/f6f03f28-67be-4725-bc24-dee94efcc6c9_hi.mp3",
+
+         video_m:
+            "https://coroverbackendstorage.blob.core.windows.net/skillindia-mobile-videos-bucket/Hi-Fallback-%E0%A4%B9%E0%A4%AE-%E0%A4%86%E0%A4%AA%E0%A4%B8%E0%A5%87-%E0%A4%B6%E0%A5%80%E0%A4%98%E0%A5%8D%E0%A4%B0-%E0%A4%AE%E0%A4%BF%E0%A4%B2%E0%A5%87%E0%A4%82%E0%A4%97%E0%A5%87.mp4",
+
+         video: true,
+
+         answerId: " ",
+      },
+   ],
+};
