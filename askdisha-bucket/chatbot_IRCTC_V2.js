c89eaa36c5eb5da49a14b6a1d456d14403918f84
Abhishek-Sundal <87581651+Abhishek-Sundal@users.noreply.github.com>
2023-10-27 12:14:59 +0530

    Update chatbot_IRCTC_V2.js

--- askdisha-bucket/chatbot_IRCTC_V2.js
+++ askdisha-bucket/chatbot_IRCTC_V2.js
@@ -1409,8 +1409,9 @@ border-bottom-right-radius: 4px;
     }, 4000);
 
     setInterval(() => {
-      if(isTrainList && !isInnerAdPushed){
+      if(!isTrainList && !isInnerAdPushed){
         pushInnerAd();
+        isTrainList = false;
       }
       if (!window.location.href.includes("irctc.co.in/nget/train-search")) {
         openBanner(false);
@@ -2386,4 +2387,3 @@ unibots.cmd.push(() => {
 </style>`
   );
 })();
-
