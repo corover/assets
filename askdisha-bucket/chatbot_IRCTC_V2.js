e033424f4f58e3d63af737e298041d42683af493
Abhishek-Sundal <abhisheksundal09@gmail.com>
2023-12-06 01:35:00 +0530

    splash train list

--- askdisha-bucket/chatbot_IRCTC_V2.js
+++ askdisha-bucket/chatbot_IRCTC_V2.js
@@ -1486,8 +1486,9 @@ border-bottom-right-radius: 4px;
       //   irctc.co.in/nget/train-search
       if (
         (!window.location.href.includes("irctc.co.in/nget/train-search") &&
-        !window.location.href.includes("irctc.co.in/nget/booking/train-list")) 
-        ) {
+        (window.location.href.includes("irctc.co.in/nget/booking/train-list")) &&
+        ifMobile
+        )) {
         openBanner(false);
         document.getElementById("askDishaSdk").style.display = "none";
         document.getElementById("div-gpt-ad-1695628181945-0").style.display =
