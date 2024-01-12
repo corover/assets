aaaf81e65baf0d20dd5ba082ef60e47520088454
Abhishek-Sundal <abhisheksundal09@gmail.com>
2024-01-12 19:12:12 +0530

    tags update

--- askdisha-bucket/chatbot_IRCTC_V2.js
+++ askdisha-bucket/chatbot_IRCTC_V2.js
@@ -1428,10 +1428,10 @@ border-bottom-right-radius: 4px;
     };
     openBanner(false);
 
-    // setTimeout(() => {
-    //   if (!isOpen && !window.location.href.includes("nget/booking/train-list"))
-    //     openBanner(true);
-    // }, 4000);
+    setTimeout(() => {
+      if (!isOpen && !window.location.href.includes("nget/booking/train-list"))
+        openBanner(true);
+    }, 4000);
 
     function minim() {
       isSmall = true;
