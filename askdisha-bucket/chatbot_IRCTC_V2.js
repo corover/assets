8bc0915cc5afbde26ae0b8e0c1b9dad3e6cea553
CoRover <41186702+corover@users.noreply.github.com>
2024-01-18 13:34:23 +0530

    Update chatbot_IRCTC_V2.js

--- askdisha-bucket/chatbot_IRCTC_V2.js
+++ askdisha-bucket/chatbot_IRCTC_V2.js
@@ -2432,8 +2432,9 @@ display:none;
 
     document.head.appendChild(videoScript);
   }
-
-  function setVideoAd(setHeader) {
+  function setVideoAd(setHeader){
+  }
+  function setVideoAd1(setHeader) {
     setVideoAdHeader();
     try {
       let leftover = document.querySelectorAll("#adContainer1")[0];
