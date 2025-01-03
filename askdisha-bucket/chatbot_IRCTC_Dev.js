e5406b6ebe8346c4951686ce5346c0d21e928547
Abhishek-Sundal <abhisheksundal09@gmail.com>
2025-01-03 12:04:05 +0530

    cross button

--- askdisha-bucket/chatbot_IRCTC_Dev.js
+++ askdisha-bucket/chatbot_IRCTC_Dev.js
@@ -1208,7 +1208,7 @@ margin-bottom: -2px;">SALE
  
      <div style="cursor:pointer;width: 100%; height: ${mediaObj.bannerHeight}; display: flex; background-color: #f6f6f6 " id="disha-image">
        <img
-         src="https://cdn.jsdelivr.net/gh/corover/assets@3jan-t1/askdisha-bucket/IRCTC-banner-crop1.gif"
+         src="https://cdn.jsdelivr.net/gh/corover/assets@3jan-t2/askdisha-bucket/IRCTC-banner-crop1.gif"
          style="width: 33%; height: 100%; border-radius: 8px"
          alt=""
          decoding = "async"
