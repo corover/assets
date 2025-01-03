20bd30c11a6ee9cd4fdbc9c502d880894aecad29
Abhishek-Sundal <abhisheksundal09@gmail.com>
2025-01-04 04:11:00 +0530

    fix

--- askdisha-bucket/chatbot_IRCTC_V2.js
+++ askdisha-bucket/chatbot_IRCTC_V2.js
@@ -595,7 +595,7 @@ const setPlaceholderMaxHeight = () => {
       }
     }
     maxIcon.onclick = (e) => {
-      // console.log("on click");
+      console.log("on click");
       // // isSmall = !isSmall;
       // e.preventDefault();
       // e.stopPropagation();
