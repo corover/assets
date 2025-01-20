d6722df3a06fd341011198bfc04d329850c34ef7
Abhishek-Sundal <abhisheksundal09@gmail.com>
2025-01-20 15:25:34 +0530

    wap DOD

--- askdisha-bucket/chatbot_IRCTC_V2.js
+++ askdisha-bucket/chatbot_IRCTC_V2.js
@@ -1454,7 +1454,7 @@ border-bottom-right-radius: 4px;
       isSmall = true;
       if (true) dealOfDay.style.display = `none`;
 
-      remove320();
+      // remove320();
       adDownIframe2.style.display = `none`;
       maxIcon.src = "https://sdk.irctc.corover.ai/askdisha-bucket/maximise.png";
       dishaWrapper.style.bottom = "200px";
